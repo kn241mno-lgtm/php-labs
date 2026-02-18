5c39cb0a993cf96f308b8d9f809cc0a2c587aa7d
Нiкiта Мiсiонжнiк <kn241_mno@student.ztu.edu.ua>
2026-02-18 15:16:59 +0200

    lr1_task5

--- lr1/variants/v14/task6.php
+++ lr1/variants/v14/task6.php
@@ -2,7 +2,7 @@
 /**
  * Завдання 5: Тризначне число
  *
- * Число 581: сума цифр=14, зворотне=185, паліндром=ні
+ * Число 368: сума цифр=17, зворотне=863, Найменше число = 368
  */
 require_once __DIR__ . '/layout.php';
 
@@ -22,13 +22,20 @@ function reverseNumber(int $number): int
     return $d3 * 100 + $d2 * 10 + $d1;
 }
 
-function isPalindrome(int $number): bool
+function getSmallestNumber(int $number): int
 {
-    return $number === reverseNumber($number);
+    $d1 = (int) floor($number / 100);
+    $d2 = (int) floor(($number % 100) / 10);
+    $d3 = $number % 10;
+
+    $digits = [$d1, $d2, $d3];
+    sort($digits);
+
+    return $digits[0] * 100 + $digits[1] * 10 + $digits[2];
 }
 
-// Вхідні дані (варіант 30)
-$number = 581;
+// Вхідні дані (варіант 14)
+$number = 368;
 
 $d1 = (int)($number / 100);
 $d2 = (int)(($number % 100) / 10);
@@ -36,9 +43,7 @@ $d3 = $number % 10;
 
 $sum = sumOfDigits($number);
 $reversed = reverseNumber($number);
-$palindrome = isPalindrome($number);
-$palindromeText = $palindrome ? "так" : "ні";
-$palindromeColor = $palindrome ? "#10b981" : "#ef4444";
+$smallest = getSmallestNumber($number);
 
 $content = '<div class="task6-container">
     <div class="card">
@@ -53,13 +58,14 @@ $content = '<div class="task6-container">
 
     <div class="card mt-20">
         <h3>📊 Результати</h3>
-        <div class="result-row">
+       <div class="result-row">
             <div>
                 <span>1. Сума цифр</span>
                 <div class="func">sumOfDigits(' . $number . ')</div>
             </div>
             <span class="result-value">' . $sum . '</span>
         </div>
+
         <div class="result-row">
             <div>
                 <span>2. Зворотне число</span>
@@ -67,14 +73,17 @@ $content = '<div class="task6-container">
             </div>
             <span class="result-value">' . $reversed . '</span>
         </div>
+
         <div class="result-row">
             <div>
-                <span>3. Паліндром?</span>
-                <div class="func">isPalindrome(' . $number . ')</div>
+                <span>3. Найменше число</span>
+                <div class="func">getSmallestNumber(' . $number . ')</div>
             </div>
-            <span class="result-value" style="color:' . $palindromeColor . '">' . $palindromeText . '</span>
+            <span class="result-value">' . $smallest . '</span>
         </div>
+
     </div>
 </div>';
+
 
 renderVariantLayout($content, 'Завдання 5', 'task6-body');
